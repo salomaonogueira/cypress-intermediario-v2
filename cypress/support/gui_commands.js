abc36264e7c68e0fc4e109e8a2402bc6806947b6
Salomão <salomao3893@gmail.com>
2025-01-06 22:47:44 -0400

    adicionando teste de criação de projeto

--- cypress/support/gui_commands.js
+++ cypress/support/gui_commands.js
@@ -18,3 +18,11 @@ Cypress.Commands.add('login', (
     cy.contains('Sign out').click()
   })
   
+  Cypress.Commands.add('gui_createProject', project => {
+    cy.visit('/projects/new')
+  
+    cy.get('#project_name').type(project.name)
+    cy.get('#project_description').type(project.description)
+    cy.get('.qa-initialize-with-readme-checkbox').check()
+    cy.contains('Create project').click()
+  })
